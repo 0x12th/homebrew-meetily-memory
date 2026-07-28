80648e5235c8f1033126c477dc4681747355d34a
Nikita Biryukov <n.biryukov12@gmail.com>
2026-07-28 21:11:46 +0300

    meetily-memory 0.5.0

--- Formula/meetily-memory.rb
+++ Formula/meetily-memory.rb
@@ -1,16 +1,16 @@
 class MeetilyMemory < Formula
   desc "Local-first Meetily history index and CLI"
   homepage "https://github.com/0x12th/meetily-memory"
-  version "0.4.1"
+  version "0.5.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/0x12th/meetily-memory/releases/download/v0.4.1/meetily-memory-v0.4.1-macos-arm64.tar.gz"
-      sha256 "39a62ae66302f1a760f51b9829442eac0ce617e96106d7d16d002fcd3cd92e23"
+      url "https://github.com/0x12th/meetily-memory/releases/download/v0.5.0/meetily-memory-v0.5.0-macos-arm64.tar.gz"
+      sha256 "108d58d3157879ca7e07b1e77d6576aaa18dfba48fc42d7098345dc0ffb85fea"
     elsif Hardware::CPU.intel?
-      url "https://github.com/0x12th/meetily-memory/releases/download/v0.4.1/meetily-memory-v0.4.1-macos-x86_64.tar.gz"
-      sha256 "ea8be595de54d37e3ad4f5c5193842fe9b74ab526e54ac1840ab02b09a9dbde2"
+      url "https://github.com/0x12th/meetily-memory/releases/download/v0.5.0/meetily-memory-v0.5.0-macos-x86_64.tar.gz"
+      sha256 "e31d57a65d4d693ae16cbf8c94d2e051e550bf972d2bba9a70a1d7db107bd5cf"
     end
   end
 
